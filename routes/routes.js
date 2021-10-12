67ffebe1e7a12f8542ed1d12ef991700130e1445
Rishabh Devbanshi <rdevbanshi35@gmail.com>
2021-10-12 13:57:53 +0530

    fixed a small bug in validator fucntion

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const checkIMEI = require("../controllers/checkIMEI");
 const route = express.Router();
 
+
+route.get("/",(req,res)=>{
+    res.send('Send Request to endpoint "<strong>/imei</strong>" to validate your IMEI number');
+})
 route.get("/imei", checkIMEI);
 
 module.exports = route;
